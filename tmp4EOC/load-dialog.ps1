c627e930f7ba0f1ecc2b099d2d2ccdc7c879562e
PhilMEFL <philippe.martin@huleu.be>
2019-01-31 18:45:42 +0100

    Add files via upload

---
+++ tmp4EOC/load-dialog.ps1
@@ -0,0 +1,25 @@
+﻿function load-dialog {
+	[CmdletBinding()]
+	param(
+ 	[Parameter(Mandatory=$True,Position=1)]
+ 	[string]$XamlPath
+	)
+ 
+	$xmlWPF = Get-Content -Path $XamlPath
+ 
+	#Add WPF and Windows Forms assemblies
+	try {
+		Add-Type -AssemblyName PresentationCore,PresentationFramework,WindowsBase,system.windows.forms
+		} 
+	catch {
+ 		Throw "Failed to load Windows Presentation Framework assemblies."
+		}
+ 
+#Create the XAML reader using a new XML node reader
+	$xamGUI = [Windows.Markup.XamlReader]::Load((new-object System.Xml.XmlNodeReader $xmlWPF))
+ 
+#Create hooks to each named object in the XAML
+	$xmlWPF.SelectNodes("//*[@Name]") | %{
+ 		Set-Variable -Name ($_.Name) -Value $xamGUI.FindName($_.Name)
+		}		
+	}
