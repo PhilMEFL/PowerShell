4617d723d7a1abc4b45e618622e8ed5b688d985c
PhilMEFL <philippe.martin@huleu.be>
2019-04-01 14:39:37 +0200

    Add files via upload

---
+++ read-xml.ps1
@@ -0,0 +1,29 @@
+﻿function read-XML ($xml) {
+    $objXML = @()
+    $xml.ChildNodes | %{
+        if (!$_.NextSibling) {
+                read-XML $_
+                }
+            else {
+                if ($_.haschildnodes) {
+                $xmlTmp = $_.ChildNodes
+                if ($_.Name -ne 'element') {
+                    $xmlTmp = $_.ParentNode.ChildNodes
+                    } 
+                $obj = '' | select ($xmlTmp|  Get-Member -MemberType Property).Name
+                ($obj | Get-Member -MemberType NoteProperty).Name | %{
+                    $obj.$_ = $xmlTmp.$_
+                    }
+                if ($obj.gtoupId -eq 56) {
+                    $toto
+                    }
+                $objXML += $obj
+                }
+            }
+        }
+       $objXML
+	   }
+
+
+                            
+                          
